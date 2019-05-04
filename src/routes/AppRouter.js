95b93b662fb625bb464bc304ad4816cb1ed818be
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-05-05 00:20:46 +0300

    App Component added

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import App from '../components/App'
 import HomePage from '../containers/HomePage'
 import { GlobalStyle } from '../styles/global'
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
-    </Switch>
-    <GlobalStyle />
+    <App>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+      </Switch>
+      <GlobalStyle />
+    </App>
   </BrowserRouter>
 )
 
