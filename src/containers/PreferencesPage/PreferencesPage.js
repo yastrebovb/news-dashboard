cc0d98c997713cda6a66a47897c216c2f96f5f86
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-05-05 09:17:47 +0300

    preferences page added

---
+++ src/containers/PreferencesPage/PreferencesPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PreferencesPage = () => <div>preferences</div>
+
+export default PreferencesPage
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import App from '../components/App'
 import HomePage from '../containers/HomePage'
+import PreferencesPage from '../containers/PreferencesPage'
 import { GlobalStyle } from '../styles/global'
 
 const AppRouter = () => (
@@ -10,6 +11,9 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" exact>
           <HomePage />
+        </Route>
+        <Route path="/preferences">
+          <PreferencesPage />
         </Route>
       </Switch>
       <GlobalStyle />
