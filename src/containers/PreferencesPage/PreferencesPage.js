692a2e67beb37e0e482854638800bd812243201e
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-05-05 15:39:40 +0300

    PreferencesPage

--- src/containers/PreferencesPage/PreferencesPage.js
+++ src/containers/PreferencesPage/PreferencesPage.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { PreferencesPageStyled, Heading, Channel } from './style'
 
-const PreferencesPage = () => <div>preferences</div>
+const PreferencesPage = ({ availableChannels, selectedChannels }) => (
+  <PreferencesPageStyled>
+    <Heading>Choose your prefered media</Heading>
+    {availableChannels.map(channel => (
+      <Channel selected={selectedChannels.includes(channel) ? true : null}>
+        {channel}
+      </Channel>
+    ))}
+  </PreferencesPageStyled>
+)
 
-export default PreferencesPage
+const mapStateToProps = state => {
+  return {
+    availableChannels: state.configuration.availableChannels,
+    selectedChannels: state.configuration.selectedChannels
+  }
+}
+
+export default connect(mapStateToProps)(PreferencesPage)
---
+++ src/containers/PreferencesPage/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const PreferencesPageStyled = styled.div`
+  padding: 40px 0;
+
+  &:before {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(110deg, #fa957b 0%, #f7496a 100%);
+    z-index: -1;
+    content: '';
+  }
+`
+
+export const Heading = styled.h2`
+  font-size: 22px;
+  text-align: center;
+`
+
+export const Channel = styled.p`
+  text-align: center;
+  color: ${props => (props.selected ? '#ecf0f1' : 'rgba(236, 240, 241, 0.6)')};
+`
