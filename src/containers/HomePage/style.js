cffe42d41e09ae717fcb2e263d6301758adcf34c
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-05-10 00:05:30 +0300

    small animation fix

--- src/containers/HomePage/style.js
+++ src/containers/HomePage/style.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import { fadeIn } from '../../styles/animations'
 
 export const HomePageStyled = styled.div`
-  &.page-enter {
-    animation: ${fadeIn} 0.3s 0.2s both;
+  &.page-enter-active {
+    animation: ${fadeIn} 0.5s 0.2s both;
   }
 
   &.page-exit-active {
